4582999c759539f5b91509438047d023ce0e6e58
Areej Anjum <strugglebird99@gmail.com>
2026-07-04 20:33:09 +0200

    fix: path in runner.py get_data_path function

--- Code/runner.py
+++ Code/runner.py
@@ -25,8 +25,7 @@ def get_data_path(config):
     data_path = Path(config["DATA_PATH"])
     if data_path.is_absolute():
         return data_path
-    return Path(__file__).resolve().parent / data_path
-
+    return Path(__file__).resolve().parent.parent / data_path
 def run_experiment(model,config, model_name,data_name, data_augmentation, device):
 
     train_loader, val_loader, test_loader = get_loaders(data_name, data_path=get_data_path(config), batch_size=config["BATCH_SIZE"], val_split=config.get("VAL_SPLIT"), seed=config.get("SEED", 42), transform=data_augmentation) 
